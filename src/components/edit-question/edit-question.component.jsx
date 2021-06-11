ce04b4d6359f13a799281bfe5dd12a0c286002d2
c1894p <csp1894@gmail.com>
2021-06-11 18:09:47 -0400

    value filled edit questions

--- src/components/edit-question/edit-question.component.jsx
+++ src/components/edit-question/edit-question.component.jsx
@@ -2,25 +2,32 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 
-export const EditQuestion = ({getData}) => {
+export const EditQuestion = ({ getData }) => {
   // {formValue, setFormValue}
   const { id } = useParams();
   const { questionID } = useParams();
-  const [questionState, setQuestionState] = useState([]);
-  const [formValue, setFormValue] = useState("");
+  const [formValue, setFormValue] = useState({});
 
-  const history = useHistory()
+  const history = useHistory();
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_BACK_END_URL}/quizzes/${id}`)
+      .get(`${process.env.REACT_APP_BACK_END_URL}/quizzes/${id}/${questionID}`)
       .then((res) => {
         return res;
       })
       .then((res) => {
         const data = res.data;
-        console.log(data.questions);
-        setQuestionState(data.questions);
+        console.log(data);
+        setFormValue(
+          { question: data.question, 
+            answer: data.answer,
+            optionA: data.options.A,
+            optionB: data.options.B,
+            optionC: data.options.C,
+            optionD: data.options.D
+          }
+          );
       })
       .catch((e) => {
         console.log("Get req error for quiz data", e);
@@ -31,18 +38,21 @@ export const EditQuestion = ({getData}) => {
     e.preventDefault();
 
     axios
-      .put(`${process.env.REACT_APP_BACK_END_URL}/quizzes/${id}/${questionID}/editquestion`, {
-        question: e.target.question.value,
-        optionA: e.target.optionA.value,
-        optionB: e.target.optionB.value,
-        optionC: e.target.optionC.value,
-        optionD: e.target.optionD.value,
-        answer: e.target.answer.value,
-      })
+      .put(
+        `${process.env.REACT_APP_BACK_END_URL}/quizzes/${id}/${questionID}/editquestion`,
+        {
+          question: e.target.question.value,
+          optionA: e.target.optionA.value,
+          optionB: e.target.optionB.value,
+          optionC: e.target.optionC.value,
+          optionD: e.target.optionD.value,
+          answer: e.target.answer.value,
+        }
+      )
       .then((res) => {
-        console.log(res.data)
-        getData()
-        history.push(`/dashboard/${id}`)
+        console.log(res.data);
+        getData();
+        history.push(`/dashboard/${id}`);
       })
       .catch((err) => console.log(err));
   };
@@ -55,73 +65,57 @@ export const EditQuestion = ({getData}) => {
   return (
     <div>
       <h1>EDIT QUESTION</h1>
-      {questionState.map((q) => {
-        return (
-          q._id === questionID && (
-            <div key={q._id}>
-              <h3>Question: {q.question}</h3>
-              <h4>Option A: {q.options.A}</h4>
-              <h4>Option B: {q.options.B}</h4>
-              <h4>Option C: {q.options.C}</h4>
-              <h4>Option D: {q.options.D}</h4>
-              <h4>Answer: {q.answer}</h4>
-
-              <form onSubmit={handleSubmit}>
-                <label htmlFor="question">Question: </label>
-                <input
-                  type="text"
-                  name="question"
-                  value={formValue.question}
-                  onChange={handleChange}
-                  required
-                />
-                <label htmlFor="optionA">Option A: </label>
-                <input
-                  type="text"
-                  name="optionA"
-                  value={formValue.optionA}
-                  onChange={handleChange}
-                  required
-                />
-                <label htmlFor="optionB">Option B: </label>
-                <input
-                  type="text"
-                  name="optionB"
-                  value={formValue.optionB}
-                  onChange={handleChange}
-                  required
-                />
-                <label htmlFor="optionC">Option C: </label>
-                <input
-                  type="text"
-                  name="optionC"
-                  value={formValue.optionC}
-                  onChange={handleChange}
-                  required
-                />
-                <label htmlFor="optionD">Option D: </label>
-                <input
-                  type="text"
-                  name="optionD"
-                  value={formValue.optionD}
-                  onChange={handleChange}
-                  required
-                />
-                <label htmlFor="answer">Enter Answer: </label>
-                <input
-                  type="text"
-                  name="answer"
-                  value={formValue.answer}
-                  onChange={handleChange}
-                  required
-                />
-
-                <input type="submit" value="Submit Change" />
-              </form>
-            </div>
-          )
-        );
-      })}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="question">Question: </label>
+        <input
+          type="text"
+          name="question"
+          value={formValue.question}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="optionA">Option A: </label>
+        <input
+          type="text"
+          name="optionA"
+          value={formValue.optionA}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="optionB">Option B: </label>
+        <input
+          type="text"
+          name="optionB"
+          value={formValue.optionB}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="optionC">Option C: </label>
+        <input
+          type="text"
+          name="optionC"
+          value={formValue.optionC}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="optionD">Option D: </label>
+        <input
+          type="text"
+          name="optionD"
+          value={formValue.optionD}
+          onChange={handleChange}
+          required
+        />
+        <label htmlFor="answer">Enter Answer: </label>
+        <input
+          type="text"
+          name="answer"
+          value={formValue.answer}
+          onChange={handleChange}
+          required
+        />
+        <input type="submit" value="Submit Change" />
+      </form>
     </div>
   );
 };
